86b84fa2ca89c086f5ef4df1dff4d9c83bad2640
Timothy Hjort <holder@localhost.localdomain>
2020-03-06 20:55:13 +0100

    Program is now ready for testing.

--- main.py
+++ main.py
@@ -7,29 +7,6 @@ import itertools
 # Dependencies: Make, gcc, Linux. python3.7
 
 #sys.argv[1] is the location of the makefile.
-if len(argv) < 2:
-    print("Usage:\nCall: python3 main.py [PATH TO DIRECTORY CONTAINING MAKEFILE] [GCC FLAGS]\nNote: This assumes the gcc flags in the makefile are passed as a variable named CXXFLAGS.\
-        \nAnd that the executable is found in PATH/bin/program.exe")
-else:
-
-    print("Cleaning filetree")
-    os.system("make clean")
-    print("Compiling program...")
-    if len(argv) > 2:
-        flags = argv[2:]
-    else:
-        # Read flags from flags.db
-        try:
-            flagsdb = open(argv[1] + "/flags.db")
-            flags = []
-            for line in flagsdb:
-                flags.append(line)
-            flagsdb.close()
-        except IOError:
-                print("Could not open flags.db and no flags passed as parameters. Exiting...")
-                flagsdb.close()
-                sys.exit(1) # EXIT_FAILURE
-
 
 def randflags(rawflags):
     """ Randomizes flags from rawflags and returns an array of variable-length combinations""" 
@@ -41,24 +18,24 @@ def randflags(rawflags):
     return flags # Should be safe even if rawflags was empty
 
 
-def log(path, result, flaglist):
+def log(path, programname, result, flaglist):
     """ Logs one result to a logfile. """
     try:
         resultLog = open(path + "/log.txt", "w")
         flags  = ""
         for flag in flaglist:
             flags += flag + " "
-        resultLog.write(path + "/Program.exe: " + str(result) + " " + flags)
+        resultLog.write(path + "/" + programname + ": " + str(result) + " " + flags)
     except IOError:
         print("Could not open logfile")
         return False
     return False
 
 
-def TimeProgram(path, rawflags):
+def TimeProgram(path, programname, rawflags):
     """ Assumes rawflags is a 2d array containing combinations of our flags """
     
-    for flaglist in flags:
+    for flaglist in rawflags:
         command = "make -j" + str(cpu_count()) + "CXXFLAGS = \""
         for flag in flaglist:
             command += flag + " "
@@ -76,7 +53,7 @@ def TimeProgram(path, rawflags):
                     # Let's run the program 5 times and log the average time.
                     t_start = datetime.now()
 
-                    os.system(path + "/bin/Program.exe") # TODO: Implement custom program name
+                    os.system(path + "/bin/" + programname)
                     # TODO: Consider program return values. End if non-zero return.
 
                     t_end = datetime.now()
@@ -96,4 +73,40 @@ def TimeProgram(path, rawflags):
             print("Something went wrong testing the program.\nFlags were: " + flagsUsed + "\nExiting...")
             return False
     return True # We tried all combinations without a hitch.
+
+
+def main():
+
+    if len(argv) < 3:
+        print("Usage:\nCall: python3 main.py [PATH TO DIRECTORY CONTAINING MAKEFILE] [EXECUTABLE NAME] [GCC FLAGS]\nNote: This assumes the gcc flags in the makefile are passed as a variable named CXXFLAGS\
+            \nand that the executable is found in PATH/bin")
+        return False
+    else:
+
+        print("Cleaning filetree")
+        os.system("make clean")
+        print("Compiling program...")
+        ProgramName = argv[2] # Saving programname
+        if len(argv) > 3:
+            flags = argv[3:]
+        else:
+            # Read flags from flags.db
+            try:
+                flagsdb = open(argv[1] + "/flags.db")
+                flags = []
+                for line in flagsdb:
+                    flags.append(line)
+                flagsdb.close()
+            except IOError:
+                    print("Could not open flags.db and no flags passed as parameters. Exiting...")
+                    flagsdb.close()
+                    return False
             
+            RandomizedFlags = randflags(flags)
+            # We are now ready to compile and run the tests.
+            TimeProgram(argv[1], ProgramName, RandomizedFlags)
+
+if __name__ == "__main__":
+    main()
+            
+
