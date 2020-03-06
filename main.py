6ff1f419ba558e1600457dc66f07821cde6bf650
Timothy Hjort <holder@localhost.localdomain>
2020-03-06 20:18:27 +0100

    Implementing a flag randomization feature

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 from sys import argv # argv[0] gives main.py
 import os  # print os.popen("echo Hello, World!").read()
 from multiprocessing import cpu_count
-# Dependencies: Make, gcc, Linux.
+import datetime
+import itertools
+# Dependencies: Make, gcc, Linux. python3.7
 
 #sys.argv[1] is the location of the makefile.
 if len(argv) < 2:
-    print("Usage:\nCall: python3 main.py [PATH TO DIRECTORY CONTAINING MAKEFILE] [GCC FLAGS]\nNote: This assumes the gcc flags in the makefile are passed as a variable named CXXFLAGS.")
+    print("Usage:\nCall: python3 main.py [PATH TO DIRECTORY CONTAINING MAKEFILE] [GCC FLAGS]\nNote: This assumes the gcc flags in the makefile are passed as a variable named CXXFLAGS.\
+        \nAnd that the filestructure of your source code is as following:\nbin/Executable\nbuild/[.o FILES]\nsrc/[.cpp FILES]\ninclude/[.hpp FILES]")
 else:
     try:
         print("Cleaning filetree")
@@ -18,7 +21,7 @@ else:
             # Read flags from flags.db
             try:
                 flagsdb = open(argv[1] + "/flags.db")
-                flags = ()
+                flags = []
                 for line in flagsdb:
                     flags.append(line)
                 flagsdb.close()
@@ -28,21 +31,29 @@ else:
                 sys.exit(1) # EXIT_FAILURE
         
         command = "make -j" + str(cpu_count()) + "CXXFLAGS = \""
-        for flag in flags:
-            command += flag + " "
-        
-        returnval = os.popen(command)
-        
-        if returnval:
-            raise OSError
-        else: # We managed to compile the program
-            #Let's import the flags.
-            if len(argv) > 2:
-                # Let's take the flags from our parameters. That is everything after the makefile path
-                flags = argv[2:]
-                
 
+def TimeProgram(path, rawflags):
+        for i in range(len(rawflags)):
+            flags = itertools.permutations(rawflags[:i])
+            for flag in flags:
+                command += flag + " "
+            command += "\""
 
-    except OSError:
-        print("Something went wrong compiling program. Exiting...")
-        
+            returnval = os.popen(command) # Compiling code using make.
+            
+            if returnval != 0: # Something went wrong when compiling
+                raise OSError
+
+            else: # We managed to compile the program
+                # Let's benchmark the program.
+                for i in range(5):
+                    # Let's run the program 5 times and log the average time.
+                    t_start = datetime.now()
+                    # RUN PROGRAM
+                    
+
+                    t_end = datetime.now()
+                    result = t_start - t_end
+        except OSError:
+            print("Something went wrong compiling program. Exiting...")
+            
